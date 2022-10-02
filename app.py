50a8b62cd38222a7a346b648fd63d5cf5ca87462
Brian Skinn <brian.skinn@gmail.com>
2022-10-01 23:03:21 -0400

    Add Tailwind and v initial styling

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import dash
 from dash import Dash, html as dhtml
 
-app = Dash("ccl-app", use_pages=True)
+external_scripts = ["https://tailwindcss.com/", {"src": "https://cdn.tailwindcss.com"}]
+
+app = Dash("ccl-app", use_pages=True, external_scripts=external_scripts)
 
 HEADER_SECTION = "layout-header"
 BODY_SECTION = "layout-body"
@@ -12,10 +14,19 @@ app.layout = dhtml.Div(
     [
         dhtml.Div(
             id=HEADER_SECTION,
-            children=[dhtml.A(id=HEADER_HOME_LINK, href="/", children="Home")],
+            children=[
+                dhtml.A(
+                    id=HEADER_HOME_LINK,
+                    href="/",
+                    children="Home",
+                    className="text-blue-700 visited:text-purple-700 hover:bg-gray-200",
+                )  # TODO: Refactor project into package and add module with classNames
+            ],
+            className="pb-4",
         ),
         dhtml.Div(id=BODY_SECTION, children=dash.page_container),
-    ]
+    ],
+    className="p-4",
 )
 
 
