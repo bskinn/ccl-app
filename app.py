6129448e399d78b297732f3da4d1ddb5b66c7474
Brian Skinn <brian.skinn@gmail.com>
2022-10-03 11:14:57 -0400

    Add COSMO theme to figures
    Have to call template load function as a member of a module,
    not imported as a standalone function, otherwise the changes to
    the plotly template config don't persist.

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import dash
 import dash_bootstrap_components as dbc
+import dash_bootstrap_templates as dbt
 from dash import Dash, html as dhtml
 
+dbt.load_figure_template("cosmo")
 
 app = Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.COSMO])
 
